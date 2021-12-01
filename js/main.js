836b6589ec2b6228f11853f7934e7246ff4c97c1
ntorguud <ntorguud@gmail.com>
2021-12-01 15:12:58 +0800

    Add showcase, introdcution, contact sections and live map

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function initMap() {
+    const loc = { 
+        lat: 47.897508,
+        lng: 106.925983
+    }
+
+    const map = new google.maps.Map (
+        document.querySelector(".map"),
+        {
+            zoom: 14,
+            center: loc
+        })
+
+    const marker = new google.maps.Marker (
+        {
+        position: loc,
+        map: map
+    })
+};
